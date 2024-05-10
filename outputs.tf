8fc7f850d93b0ef48c4ff4051c90fe2d252342c4
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-10 10:38:45 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,20 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+# Define outputs to expose important information about the EKS cluster
 output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane"  // Description of the output
+  value       = module.eks.cluster_endpoint       // Value retrieved from the EKS module
 }
 
 output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+  description = "Security group ids attached to the cluster control plane"  // Description of the output
+  value       = module.eks.cluster_security_group_id  // Value retrieved from the EKS module
 }
 
 output "region" {
-  description = "AWS region"
-  value       = var.region
+  description = "AWS region"  // Description of the output
+  value       = var.region     // Value retrieved from the input variable
 }
 
 output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
+  description = "Kubernetes Cluster Name"  // Description of the output
+  value       = module.eks.cluster_name     // Value retrieved from the EKS module
 }
