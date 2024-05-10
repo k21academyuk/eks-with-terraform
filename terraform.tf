e8e7807f823e817b7a195f45a91d1f030632ee5a
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-10 10:40:58 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,36 +1,29 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+# Define the Terraform configuration block
 terraform {
 
-  # cloud {
-  #   workspaces {
-  #     name = "learn-terraform-eks"
-  #   }
-  # }
-
+  # Define required providers and their versions
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.47.0"
+      source  = "hashicorp/aws"   # Source of the AWS provider
+      version = "~> 5.47.0"        # Version constraint for the AWS provider
     }
 
     random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6.1"
+      source  = "hashicorp/random"  # Source of the random provider
+      version = "~> 3.6.1"           # Version constraint for the random provider
     }
 
     tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.5"
+      source  = "hashicorp/tls"   # Source of the TLS provider
+      version = "~> 4.0.5"        # Version constraint for the TLS provider
     }
 
     cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.4"
+      source  = "hashicorp/cloudinit"  # Source of the cloudinit provider
+      version = "~> 2.3.4"             # Version constraint for the cloudinit provider
     }
   }
 
-  required_version = "~> 1.3"
+  # Specify the required Terraform version
+  required_version = "~> 1.3"  # Version constraint for Terraform itself
 }
-
