408036f92986a83a1296bf8281f6120e92ee0b70
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-10 10:43:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,6 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+# Define a variable named "region"
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-2"
+  description = "AWS region"  # Description of the variable
+  type        = string         # Type constraint for the variable (string)
+  default     = "us-east-2"   # Default value for the variable
 }
